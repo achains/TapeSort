9fbcbc9dc6f48009af3ad10c5431c347ac73befc
achains <arthur.salio@gmail.com>
2023-10-07 15:21:30 +0300

    feat: external sort

--- include/tapetools/sort/tape_sort.h
+++ include/tapetools/sort/tape_sort.h
@@ -4,22 +4,29 @@
 #include <queue>
 #include <vector>
 
-#include "buffer_block.h"
 #include "tapetools/tape.h"
+#include "tapetools/tape_creator.h"
 
 namespace tapetools {
 class TapeSort {
  public:
-  TapeSort(std::shared_ptr<Tape> input_tape, std::shared_ptr<Tape> output_tape, size_t max_buffer_size);
+  TapeSort(std::shared_ptr<Tape> input_tape, std::shared_ptr<Tape> output_tape,
+           std::shared_ptr<TapeCreator> tape_generator, size_t block_size, size_t max_buffer_size);
 
   void sort();
 
  private:
+  const std::string tmp_data_dir_ = "tmp";
+  size_t block_size_;
   size_t max_buffer_size_;
-  std::priority_queue<std::unique_ptr<BufferBlock>> buffer_queue_;
+  std::shared_ptr<TapeCreator> tape_generator_;
   std::shared_ptr<Tape> input_tape_;
   std::shared_ptr<Tape> output_tape_;
 
-  void writeSortedBlock(std::vector<int> const& sorted_block);
+  size_t generateSortedTemporaryTapes();
+
+  bool merge(std::vector<size_t> const& merge_candidates_id, size_t merge_tape_id);
+
+  std::unique_ptr<Tape> createTemporaryTape(size_t tape_id) const;
 };
 }  // namespace tapetools
--- src/tapetools/sort/buffer_block.cpp
+++
@@ -1,9 +0,0 @@
-#include "tapetools/sort/buffer_block.h"
-
-namespace tapetools {
-BufferBlock::BufferBlock(std::vector<int> const& raw_data) : data_(raw_data) {}
-
-bool BufferBlock::hasNext() const { return current_pos_ < data_.size(); }
-
-int BufferBlock::getNext() { return data_[current_pos_++]; }
-}  // namespace tapetools
